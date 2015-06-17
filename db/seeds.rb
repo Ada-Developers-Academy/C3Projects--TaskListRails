de3255cff4a3d5b0842d20dc7a5dc42535ea08a4
Loraine <loraine@lorainekv.com>
2015-06-17 15:32:12 -0700

    Generated new controller and made a view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-tasks = [{taskname: "Rails for zombies", description: "Finish level 1.", datecomp: "6/16"},
+checklist = [{taskname: "Rails for zombies", description: "Finish level 1.", datecomp: "6/16"},
   {taskname: "Task project", description: "Submit pr", datecomp: "6/15"},
   {taskname: "Submit surveys", description: "Those two companies.", datecomp: "6/15"}]
 
-5.times do
-  Task.create(taskname: "Git commit!", description: "Do this everytime before you stand up.")
+checklist.each do |task|
+  Task.create(task)
 end
