c3d86c5e788fe93b5a051aa8ba82350893e8018e
Ashley Watkins <AWatkins31@gmail.com>
2015-06-17 16:08:18 -0700

    Seeds the DB with 9 tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def random_time
+  Time.at(rand * Time.now.to_i)
+end
+
+seed_tasks = [
+  { name: "Breakfast", description: "", completed_at: random_time },
+  { name: "Second breakfast", description: "" },
+  { name: "Elevenses", description: "Comes after 2nd breakfast but before lunch"},
+  { name: "Luncheon", description: "", completed_at: random_time},
+  { name: "Afternoon tea", description: ""},
+  { name: "Dinner", description: "", completed_at: random_time },
+  { name: "Supper", description: "" },
+  { name: "Call Mom", description: "wish her a happy birthday", completed_at: random_time },
+  { name: "Walk Bentley", description: "Loring Park" },
+]
+
+seed_tasks.each do |task|
+  Task.create task
+end
