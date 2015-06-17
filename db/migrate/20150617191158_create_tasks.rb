cc0b7f32c5278ef71cd254e3272a4bd1274f5c8b
Elsa <elsa.moluf@gmail.com>
2015-06-17 15:17:37 -0700

    Seeded db with initial tasks.

--- db/migrate/20150617191158_create_tasks.rb
+++ db/migrate/20150617191158_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.string :name, null: false
       t.string :description
-      t.datetime :completed_date
+      t.datetime :completed_at
 
       t.timestamps null: false
     end
