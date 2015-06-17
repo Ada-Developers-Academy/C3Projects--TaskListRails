467e0ee3ded8280677f447e9178a7976a253bb43
Loraine <loraine@lorainekv.com>
2015-06-17 13:38:20 -0700

    Messing around with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+tasks = [{taskname: "Rails for zombies", description: "Finish level 1.", datecomp: "6/16"},
+  {taskname: "Task project", description: "Submit pr", datecomp: "6/15"},
+  {taskname: "Submit surveys", description: "Those two companies.", datecomp: "6/15"}]
+
+5.times do
+  Task.create(taskname: "Git commit!", description: "Do this everytime before you stand up.")
+end
