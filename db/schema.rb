d7b58d7d3823a2e53aad2387251bc588dc15adb6
Ashley Watkins <AWatkins31@gmail.com>
2015-06-17 15:18:42 -0700

    Changes column name: :completed_on -> :completed_at

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617202703) do
+ActiveRecord::Schema.define(version: 20150617223138) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "completed_on"
+    t.datetime "completed_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
