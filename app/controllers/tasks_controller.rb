4c4c080e0453245bb8a083c57bc3fdbf5d2143e8
Ashley Watkins <AWatkins31@gmail.com>
2015-06-18 16:00:05 -0700

    Updates to include new things I learned from `rails g scaffold_controller` :)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,77 @@
 class TasksController < ApplicationController
+  # set_task will be called before show, edit, update, or destroy
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
   def index
     @all_tasks = Task.all
     # render :index
   end
 
+  # GET /tasks/:id
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     render :task_details
   end
 
+  # GET /tasks/new
   def new
-
-    render :add_task
+    @task = Task.new
+    render :new_task # return an HTML form for creating a new task
   end
+
+  # # GET /tasks/:id/edit  tasks#edit return an HTML form for editing a task
+  # def edit
+  # end
+
+  # # POST /tasks
+  # # create a new task
+  # def create
+  #   @task = Task.new(task_params)
+
+  #   respond_to do |format|
+  #     if @task.save
+  #       format.html { redirect_to @task, notice: 'Task was successfully created.' }
+  #       format.json { render :show, status: :created, location: @task }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @task.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # PATCH/PUT /tasks/:id
+  # # update a specific task
+  # def update
+  #   respond_to do |format|
+  #     if @task.update(task_params)
+  #       format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @task }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @task.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # DELETE /tasks/:id
+  # delete a specific task
+  # def destroy
+  #   @task.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # # Never trust parameters from the scary internet, only allow the white list through.
+    # def task_params
+    #   params[:task]
+    # end
 end
-
-  # HTTPverb, Path, Controller#Action, Used for
-  # GET /tasks tasks#index  display a list of all tasks
-  # GET /tasks/new tasks#new  return an HTML form for creating a new task
-  # POST /tasks tasks#create create a new task
-  # GET /tasks/:id tasks#show display a specific task
-  # GET /tasks/:id/edit  tasks#edit return an HTML form for editing a task
-  # PATCH/PUT /tasks/:id tasks#update update a specific task
-  # DELETE  /tasks/:id tasks#destroy  delete a specific task
-
