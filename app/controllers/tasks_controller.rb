e391355a559c8eb0e9d2e57d08fe09aed92de4f6
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-18 07:44:08 -0700

    Edited in a comment question on task_controller. Wave 1 finished.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @all_tasks = Task.all   # NOTE TO SELF: "task" is actually a Task::ActiveRecord_Relation object, but responds to Array methods
     @completed = "COMPLETED"
@@ -9,6 +10,7 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+    # QUESTION: Is it possible to make the below instances variables available to both index & show? (A constant didn't work...)
     @completed = "COMPLETED"
     @uncompleted = "UNCOMPLETED"
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
 class Task < ActiveRecord::Base
-  def self.all_tasks
-    tasks = Task.all
-  end
+
 end
