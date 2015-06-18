74056759d66e0adaf07cb3688800e138d046348b
Loren Wang <wang131.131@gmail.com>
2015-06-18 09:36:13 -0700

    changed file names

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'task_lists#index'
-  # get '/' => 'tasklist#index'
+
+  # get '/' => 'task_lists#index'
   # Example of regular route:
   get 'task_lists/:id' => 'task_lists#show'
     # get 'products/:id' => 'catalog#view'
     ### /products/1 would get matched #first route it matches it takes starting at the top. exact match = must
     # / products wouold not
     # in this view action, expect params[:id] to exist which gets passed in through the root
-
+    # get '/new' => 'task_lists#new'.. Better Errors example
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
