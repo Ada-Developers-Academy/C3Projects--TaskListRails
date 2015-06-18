1ac632353cfdb92bf83b13bb41c7c078a081ec33
Elsa <elsa.moluf@gmail.com>
2015-06-17 18:22:10 -0700

    Wave 1 functionality with some CSS.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
-
     @all_tasks = Task.all
-
     render :index
-
   end
+
+  def show
+    @one_task = Task.find(params[:id])
+    render :show
+  end
+
 end
