2b32a02aa516778235a4a8e74ca3188c6b46cb2a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-17 18:36:59 -0700

    removed unnecessary variables on show view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,14 @@
 class TasksController < ApplicationController
   def index
   	@title = "Task List"
+  	
   	@all_tasks = Task.all
   end
 
   def show
+  	@title			= @task.name
+
   	@task 			= Task.find(params[:id])
-  	@name			= @task.name
-  	@title			= @name
-  	@description	= @task.description
-  	@completed 		= @task.completed?
   	@date			= @task.date_completed ? @task.date_completed.strftime("%m-%d-%Y") : nil
   end
 end
