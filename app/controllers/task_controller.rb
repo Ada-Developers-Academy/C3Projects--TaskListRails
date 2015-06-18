37d0bb59fa8eb6c880e586744b3fcdd7c76fa45b
Loraine <loraine@lorainekv.com>
2015-06-17 17:51:45 -0700

    Got deets to display.

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,4 +4,11 @@ class TaskController < ApplicationController
     render :index # This isn't necessary since it's implicit. However
     # we should prefer clarity.
   end
+
+  def show
+    @id = params[:id]
+    @onedeet = Task.find(@id)
+    render :show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root 'task#index'
 
+  get '/tasks/:id' => 'task#show'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
