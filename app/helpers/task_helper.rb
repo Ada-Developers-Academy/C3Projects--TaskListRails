5a245ce8afd17e39384e15d55624bdd5b38684f2
Ashley Watkins <AWatkins31@gmail.com>
2015-06-17 19:21:19 -0700

    Changed appearance of task details page

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -15,14 +15,19 @@ module TaskHelper
 
   def format_task_for_view(task)
     html = ""
-    html << "<div"
-    html << " class='completed_task'" if task.completed_at
-    html << "><p>" +
+    # html << "<div"
+    # html << " class='completed_task'" if task.completed_at
+    # html << ">"
+    html << "<p>" +
             "<span class='task_name'>#{ task.name }</span>"
-    html << "<span class='complete_date'>Completed: #{ task.completed_at.to_formatted_s(:long) }</span>" if task.completed_at
+    if task.completed_at
+      html << "<span class='complete_date'>Completed: #{ task.completed_at.to_formatted_s(:long) }</span>"
+    else
+      html << "<span class='complete_date'> Not Completed</span>"
+    end
     html << "</p>"
     html << "<p class='task_descr'> - #{task.description}</p>" if task.description
-    html << "</div>"
+    # html << "</div>"
     html.html_safe
   end
 
