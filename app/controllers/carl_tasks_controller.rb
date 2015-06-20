6a5db2199766096b603beb4d0570c0f615f1dfa0
Jeri <drvonnjerryxlii@gmail.com>
2015-06-19 17:16:31 -0700

    pretty sure i've finished wave 3. obviously i need to work on committing more often

--- app/controllers/carl_tasks_controller.rb
+++ app/controllers/carl_tasks_controller.rb
@@ -1,85 +1,72 @@
 require "time"
 
 class CarlTasksController < ApplicationController
-  def index
-    @tasks = Task.all
-  end
-
-  def task
-    if params[:id]
-      @task = Task.find(params[:id])
-    else
-      redirect_to "/"
-    end
-  end
-
+  #--------------- PARTIALS WITH LITTLE TO NO BUSINESS LOGIC -------------------
   def about
   end
 
   def four_oh_four
   end
 
-  def complete
-    if params[:id]
-      @task = Task.find(params[:id])
-    else
-      @tasks = Task.all
-    end
+  def index
+    @tasks = Task.all
   end
 
-  def create
-    if params[:id]
-      @task = Task.find(params[:id])
-    else
-      @tasks = Task.all
-    end
+  def task
+    @task = Task.find(params[:id])
+  rescue
+    redirect_to "/"
+  end
 
-    @tasky = Task.new
+  #-------------------------- CREATING A NEW TASK ------------------------------
+  def create
+    @task = Task.new
   end
 
   def created
-    task = task_params[:task]
-    name, description = task[:name], task[:description]
+    task = prepare_task_from_params
 
-    date = date_params[:start_date]
-    date_complete = (task[:complete] == "1") ? Date.parse("#{ date[:year] }/#{ date[:month] }/#{ date[:day] }") : ""
-
-    if task[:id]
-      id = task[:id]
-
-      Task.find(id).update(name: name, description: description, date_complete: date_complete)
-
-    else
-      task.except!(:complete)
-
-      if date_complete != ""
-        task[:date_complete] = date_complete
-      end
-
-      Task.create(task)
-
-      id = Task.last.id
-    end
+    Task.create(task)
+    id = Task.last.id
 
     redirect_to "/task/#{ id }"
   end
 
-  def update
-    if params[:id]
-      @task = Task.find(params[:id])
-    # else
-    #   @tasks = Task.all
-    end
+  #----------------------------- EDITING A TASK --------------------------------
+  def edit
+    @task = Task.find(params[:id])
 
     render :create
+  rescue
+    redirect_to "/edit_what_task"
   end
 
-  def delete
+  def edited
+    task = prepare_task_from_params
+    id = task[:id]
+
+    task.except![:id]
+
+    Task.find(id).update(task)
+
+    redirect_to "/task/#{ id }"
+  end
+
+  #--------------------------- COMPLETING A TASK -------------------------------
+  def complete
     if params[:id]
-      @task = Task.find(params[:id])
-    # else
-    #   @tasks = Task.all
+      task = Task.find(params[:id])
+      task.date_complete ? task.update(date_complete: nil) : task.update(date_complete: Time.now)
+
+      redirect_to "/"
     end
+  end
+
+  #---------------------------- DELETING A TASK --------------------------------
+  def delete
+    @task = Task.find(params[:id])
+  rescue
+    redirect_to "/delete_what_task"
   end
 
   def deleted
@@ -90,12 +77,32 @@ class CarlTasksController < ApplicationController
     end
   end
 
+  #---------------------------- PRIVATE METHODS --------------------------------
   private
 
+  # prepare params for create & edit methods
+  def prepare_task_from_params
+    task = task_params[:task]
+    name, description = task[:name], task[:description]
+
+    date = date_params[:start_date]
+    date_complete = (task[:complete] == "1") ? Date.parse("#{ date[:year] }/#{ date[:month] }/#{ date[:day] }") : ""
+
+    task.except!(:complete)
+
+    if date_complete != ""
+      task[:date_complete] = date_complete
+    end
+
+    return task
+  end
+
+  # permit these params nested inside task
   def task_params
     params.permit(task: [:id, :name, :description, :complete])
   end
 
+  # permit these params nested inside start_date
   def date_params
     params.permit(start_date: [:year, :month, :day])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,81 +1,33 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'carl_tasks#index'
 
-  get "/about" => "carl_tasks#about"
+  # GETs / regular links
+  get "/about" => "carl_tasks#about" # about carl
 
-  get "/task/:id" => "carl_tasks#task"
-  get "/task" => "carl_tasks#task"
+  # GETs for SPECIFIC INDIVIDUAL tasks
+  get "/task/:id" => "carl_tasks#task" # review a specific task
+  get "/edit/:id" => "carl_tasks#edit" # edit a specific task
+  get "/delete/:id" => "carl_tasks#delete"# delete a specific task
 
+
+  # GET a new task
   get "/create" => "carl_tasks#create"
+
+
+  # POST the new task
   post "/created" => "carl_tasks#created"
 
-  get "/complete/:id" => "carl_tasks#complete"
-  get "/complete" => "carl_tasks#complete"
-  # post "/complete" => "carl_tasks#post_complete"
 
-  get "/edit/:id" => "carl_tasks#create"
-  get "/edit" => "carl_tasks#create"
-  # post "/edit" => "carl_tasks#post_create"
+  # PATCHes to update tasks that already exist
+  patch "/complete/:id" => "carl_tasks#complete"
+  patch "/edited/:id" => "carl_tasks#edited"
 
-  get "/delete/:id" => "carl_tasks#delete"
-  get "/delete" => "carl_tasks#delete"
+
+  # DELETE to delete a task forever
   delete "/deleted/:id" => "carl_tasks#deleted"
 
 
-  get "/four_oh_four" => "carl_tasks#four_oh_four"
+  # GETs additional 404 handling
   get "/:anything_else" => "carl_tasks#four_oh_four"
-
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get "/:anything_else/:even_more_anything_else" => "carl_tasks#four_oh_four"
 end
