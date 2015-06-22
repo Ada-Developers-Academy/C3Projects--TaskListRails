6df800b45123575d6dba0a290e99294cec7083df
Shanna Chau <shannachau@gmail.com>
2015-06-22 15:34:12 -0700

    Index for people shows count of uncompleted tasks

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,13 @@
 class PeopleController < ApplicationController
   def index
     @people = Person.all
-
+    
   end
 
   def show
     @person = Person.find(params[:person_id])
-    
 
+    # add in logic to get
     render :person_tasks
   end
 end
