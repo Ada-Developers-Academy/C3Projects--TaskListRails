a799c2ffa212176110ee5e07c9f7125e29571722
Ashley Watkins <AWatkins31@gmail.com>
2015-06-22 14:44:30 -0700

    Removes unnecessary comments

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  # set_task will be called before show, edit, update, or destroy
   before_action :set_task, only: [:show, :edit, :update, :complete, :destroy]
 
   # GET /tasks
@@ -10,14 +9,13 @@ class TasksController < ApplicationController
 
   # GET /tasks/:id
   def show
-    # @task = Task.find(params[:id])
     render :show_task
   end
 
   # GET /tasks/new
   def new
     @task = Task.new
-    render :new_task # return an HTML form for creating a new task
+    render :new_task
   end
 
   # # GET /tasks/:id/edit  tasks#edit return an HTML form for editing a task
@@ -25,21 +23,18 @@ class TasksController < ApplicationController
   # end
 
   # POST /tasks
-  # create a new task
   def create
     # TODO: require task name from the user
     @task = Task.new(task_params)
 
     if @task.save
-      # redirect_to @task # ash prefers this
-      redirect_to tasks_path # required for project requirements
+      redirect_to tasks_path
     else
       render :new_task
     end
   end
 
   # PATCH/PUT /tasks/:id
-  # update a specific task
   def update
     if params[:complete]
       @task.change_complete_status(params[:complete])
@@ -51,20 +46,17 @@ class TasksController < ApplicationController
     end
   end
 
-  # # DELETE /tasks/:id
-  # delete a specific task
+  # DELETE /tasks/:id
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: "\"#{@task.name}\" was deleted."
   end
 
   private
-    # use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.permit(task: [:name, :description])[:task]
     end
