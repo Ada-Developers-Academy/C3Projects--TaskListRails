23068bf8ff402f7e3d621386a7374364e1a53258
Ashley Watkins <AWatkins31@gmail.com>
2015-06-21 23:44:29 -0700

    Adds (un)complete task methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ActiveRecord::Base
+  def change_complete_status(complete_request)
+    complete_request == "1" ? complete : uncomplete
+  end
 
+  def complete
+    update(completed_at: Time.now)
+  end
+
+  def uncomplete
+    update(completed_at: nil)
+  end
 end
