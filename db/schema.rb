b72b9c0c249912d33fbdfc2407dfddb8fa536b2a
Lindsey Reno <lindseyannereno@gmail.com>
2015-06-22 17:55:26 -0700

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623003720) do
+ActiveRecord::Schema.define(version: 20150623005306) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
