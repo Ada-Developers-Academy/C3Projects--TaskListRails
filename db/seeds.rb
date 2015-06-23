2a7c1a5d78cd386da4c07d8c822c52adf73f781b
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-06-22 20:26:51 -0700

    incomplete tasks on person view working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def random_time
-  Time.at(rand * Time.now.to_i)
-end
 
 tasks = [
-{  name: "The First Task", description: "", completed: random_time},
+{  name: "The First Task", description: "", completed: "Yes"},
 {  name: "Go to Brunch", description: ""},
-  {name: "Go to Lunch", description: "", completed: random_time},
+  {name: "Go to Lunch", description: "", completed: "No"},
   {name: "Go to Second Lunch", description: ""},
-  {name: "Play Video Games", description: "", completed: random_time},
-{name: "High Five Somebody You Don't Know", description: "", completed: random_time},
+  {name: "Play Video Games", description: "", completed: "Yes"},
+{name: "High Five Somebody You Don't Know", description: "", completed: "No"},
   {name: "Plant Flowers", description: "", completed: random_time},
 {    name: "Call Mom", description: ""},
   {name: "She worries, you know.", description: ""},
