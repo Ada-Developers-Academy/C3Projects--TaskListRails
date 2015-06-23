25f6f3150bff2ce1d8533b31ef09e0d608c7ad8d
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-22 22:30:11 -0700

    update seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,6 @@ def random_time
   Time.at(rand * Time.now.to_i)
 end
 
-new_tasks = [
-  { name: "The First Task", description: "do something cool", date_completed: random_time },
-  { name: "Go to Brunch", description: "eat Eggs Benedict" } ,
-  { name: "Go to Lunch", description: "eat a sandwich", date_completed: random_time },
-  { name: "Go to Second Lunch", description: "eat some tacos" },
-  { name: "Play Video Games", description: "Zelda all the way", date_completed: random_time} ,
-  { name: "High Five Somebody You Don't Know", description: "like, a stranger", date_completed: random_time },
-  { name: "Plant Flowers", description: "", date_completed: random_time },
-  { name: "Call Mom", description: "on the telephone" },
-  { name: "She worries, you know.", description: "you know she does" },
-  { name: "Nap.", description: "super hard", date_completed: random_time}
-]
-
-new_tasks.each do |task|
-  Task.create task
-end
-
 new_people = [
   { name: "Jack" },
   { name: "Samantha" },
@@ -28,4 +11,21 @@ new_people = [
 
 new_people.each do |person|
   Person.create person
+end
+
+new_tasks = [
+  { name: "The First Task", description: "do something cool", date_completed: random_time, person_id: 2 },
+  { name: "Go to Brunch", description: "eat Eggs Benedict", person_id: 3 } ,
+  { name: "Go to Lunch", description: "eat a sandwich", date_completed: random_time, person_id: 1 },
+  { name: "Go to Second Lunch", description: "eat some tacos", person_id: 4 },
+  { name: "Play Video Games", description: "Zelda all the way", date_completed: random_time, person_id: 1} ,
+  { name: "High Five Somebody You Don't Know", description: "like, a stranger", date_completed: random_time, person_id: 2 },
+  { name: "Plant Flowers", description: "", date_completed: random_time, person_id: 3 },
+  { name: "Call Mom", description: "on the telephone", person_id: 4 },
+  { name: "She worries, you know.", description: "you know she does", person_id: 1 },
+  { name: "Nap.", description: "super hard", date_completed: random_time, person_id: 2}
+]
+
+new_tasks.each do |task|
+  Task.create task
 end
