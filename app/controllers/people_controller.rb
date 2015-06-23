044c8be068a8b59180037a11062570953de2ade3
Shanna Chau <shannachau@gmail.com>
2015-06-22 20:43:24 -0700

    Added navigation buttons to go back to User page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,13 @@
 class PeopleController < ApplicationController
   def index
     @people = Person.all
-    
+
   end
 
   def show
     @person = Person.find(params[:person_id])
+    @tasks = Task.where(person_id: @person.id)
 
-    # add in logic to get
     render :person_tasks
   end
 end
