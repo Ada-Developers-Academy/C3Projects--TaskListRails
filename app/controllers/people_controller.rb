a18e59f9fd22865c56705813ab934e13fc4b3ccb
Ashley Watkins <AWatkins31@gmail.com>
2015-06-22 22:50:20 -0700

    Creates full person functionality - show, create, edit, add task, delete, etc.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,45 @@
 class PeopleController < ApplicationController
   before_action :set_person, only: [ :show, :edit, :update, :complete, :destroy ]
 
+  def index
+    @all_people = Person.all
+  end
+
   def show
   end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+    @all_people = Person.all
+  end
+
+  def create
+    # TODO: require person name from the user
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to people_path
+    else
+      render :new, notice: 'Sorry, something went wrong =('
+    end
+  end
+
+  def update
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render :edit, notice: 'Sorry, something went wrong =('
+    end
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to people_path, notice: "\"#{ @person.name }\" was deleted."
+  end
+
 
   private
     def set_person
