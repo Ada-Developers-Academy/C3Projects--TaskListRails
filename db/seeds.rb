b1ec7a68ea30105ab1adeb95246171b91b27adf7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-22 17:27:12 -0700

    full seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-# def random_time
-#   Time.at(rand * Time.now.to_i)
-# end
+def random_time
+  Time.at(rand * Time.now.to_i)
+end
 
-# new_tasks = [
-#   { name: "The First Task", description: "do something cool", date_completed: random_time },
-#   { name: "Go to Brunch", description: "eat Eggs Benedict" } ,
-#   { name: "Go to Lunch", description: "eat a sandwich", date_completed: random_time },
-#   { name: "Go to Second Lunch", description: "eat some tacos" },
-#   { name: "Play Video Games", description: "Zelda all the way", date_completed: random_time} ,
-#   { name: "High Five Somebody You Don't Know", description: "like, a stranger", date_completed: random_time },
-#   { name: "Plant Flowers", description: "", date_completed: random_time },
-#   { name: "Call Mom", description: "on the telephone" },
-#   { name: "She worries, you know.", description: "you know she does" },
-#   { name: "Nap.", description: "super hard", date_completed: random_time}
-# ]
+new_tasks = [
+  { name: "The First Task", description: "do something cool", date_completed: random_time },
+  { name: "Go to Brunch", description: "eat Eggs Benedict" } ,
+  { name: "Go to Lunch", description: "eat a sandwich", date_completed: random_time },
+  { name: "Go to Second Lunch", description: "eat some tacos" },
+  { name: "Play Video Games", description: "Zelda all the way", date_completed: random_time} ,
+  { name: "High Five Somebody You Don't Know", description: "like, a stranger", date_completed: random_time },
+  { name: "Plant Flowers", description: "", date_completed: random_time },
+  { name: "Call Mom", description: "on the telephone" },
+  { name: "She worries, you know.", description: "you know she does" },
+  { name: "Nap.", description: "super hard", date_completed: random_time}
+]
 
-# new_tasks.each do |task|
-#   Task.create task
-# end
+new_tasks.each do |task|
+  Task.create task
+end
 
 new_people = [
   { name: "Jack" },
