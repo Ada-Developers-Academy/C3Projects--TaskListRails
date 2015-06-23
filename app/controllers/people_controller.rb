d5eb62db19e66b3944d39b0148c1619e97f59730
Ashley Watkins <AWatkins31@gmail.com>
2015-06-22 22:12:14 -0700

    Creates initial private methods

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,15 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [ :show, :edit, :update, :complete, :destroy ]
+
+  def show
+  end
+
+  private
+    def set_person
+      @person = Person.find(params[:id])
+    end
+
+    def person_params
+      params.permit(person: [:name])[:person]
+    end
 end
