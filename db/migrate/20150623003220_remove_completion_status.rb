b94147eede69c920cd9c55982324d28b731731af
Lindsey Reno <lindseyannereno@gmail.com>
2015-06-22 17:48:15 -0700

    WIP

--- db/migrate/20150623003220_remove_completion_status.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCompletionStatus < ActiveRecord::Migration
-  def change
-    remove_column :tasks, :completion_status
-  end
-end
--- db/migrate/20150623003720_change_completion_status.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCompletionStatus < ActiveRecord::Migration
-  def change
-    change_column :tasks, :completion_status, :string
-  end
-end
